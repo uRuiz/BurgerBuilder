91651894b99ba8280dd2ad5b30bb1f77967e0e03
uRuiz <zaszur@gmail.com>
2019-01-18 17:35:24 +0100

    Drawing Burger Ingredients

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-import classes from './burgerIngrdient.css';
+import classes from './BurgerIngredient.css';
 
-class burgerIngrdient extends Component {
+class burgerIngredient extends Component {
+
     render () {
         let ingredient = null;
 
-        switch (props.type) {
+        switch (this.props.type) {
             case ('bread-bottom'): 
-                ingredient = <div className={classess.BreadBottom}></div>;
+                ingredient = <div className={classes.BreadBottom}></div>;
                 break;
             case ('bread-top'):
                 ingredient = (
@@ -39,4 +40,8 @@ class burgerIngrdient extends Component {
     }
 }
 
-export default burgerIngrdient;
+burgerIngredient.propTypes = {
+    type: PropTypes.string.isRequired
+};
+
+export default burgerIngredient;
